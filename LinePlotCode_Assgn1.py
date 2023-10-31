62fc03ca001092593b7590ce3e3eb056f9d0f523
SaiMohanBatta <149528797+SaiMohanBatta@users.noreply.github.com>
2023-10-31 20:07:38 +0000

    Update LinePlotCode_Assgn1.py
    Made changes to the Comments

--- LinePlotCode_Assgn1.py
+++ LinePlotCode_Assgn1.py
@@ -12,14 +12,14 @@ def Create_LinePlot(PathToFile):
 
     # Extracting the data for plotting
     country_codes = data['Country Code'] # Fetched the country codes from CSV
-    years = data.columns[3:]  # the columns of years starts for 3 onwards
+    years = data.columns[3:]  # the columns of years starts from 3rd column.
 
     # Creating a line plot with multiple lines based on Number of countries 
     plt.figure(figsize=(10, 6))
     for idx in range(len(country_codes)):
         plt.plot(years, data.iloc[idx, 3:],label=country_codes[idx])
 
-    # Adding title and labels
+    # Adding plot title and Axis labels
     plt.title('Infant Mortality Rate in Major Europe Countries from 1970 to 2020')
     plt.xlabel('Years') 
     plt.ylabel('Infant Mortality rate (per 1,000 live births)')
@@ -30,6 +30,6 @@ def Create_LinePlot(PathToFile):
     # Display the plot
     plt.show()
 
-# Replace 'path_to_your_csv_file.csv' with the actual path to your CSV file
+# Calling function to generate the Line Plot
 PathToFile = r'C:\Users\Avinash Reddy\Videos\Harsha Assignment\reduceddata.csv'
 Create_LinePlot(PathToFile)
