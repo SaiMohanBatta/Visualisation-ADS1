eb0993f644192fccda46ad029371b48bfed1957b
SaiMohanBatta <149528797+SaiMohanBatta@users.noreply.github.com>
2023-11-02 19:02:01 +0000

    Update PieChartCode_Assgn1.py
    changed the code parameters based on the data

--- PieChartCode_Assgn1.py
+++ PieChartCode_Assgn1.py
@@ -7,17 +7,17 @@ def CreatePieChart(PathToFile):
     data = pd.read_csv(PathToFile)
 
     # Extract data for the specific year
-    year_data = data.iloc[:,7]
+    percent = data.iloc[:,1]
 
     # Extract country codes
-    country_codes = data['Country Code']
+    typeofFood = data['Food']
 
     # Creating a pie chart
     plt.figure(figsize=(8, 6))
-    plt.pie(year_data, labels=country_codes)
-    plt.title(f'Pie Chart for Year 1980')
+    plt.pie(percent, labels=typeofFood)
+    plt.title('Recommended Diet Chart')
     plt.show()
 
 # Replace 'path_to_your_csv_file.csv' with the actual path to your CSV file
-PathToFile = r'C:\Users\Avinash Reddy\Videos\Harsha Assignment\reduceddata.csv'
+PathToFile = r'C:\Users\Avinash Reddy\Videos\ADS1Assgn\Diet_Chart.csv'
 CreatePieChart(PathToFile)
