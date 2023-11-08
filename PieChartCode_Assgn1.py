1a4f5313d55a44746d194a33eb9da91ab87cce64
SaiMohanBatta <149528797+SaiMohanBatta@users.noreply.github.com>
2023-11-08 20:33:19 +0000

    Update PieChartCode_Assgn1.py
    made changes as per good coding style and PEP-8

--- PieChartCode_Assgn1.py
+++ PieChartCode_Assgn1.py
@@ -1,23 +1,28 @@
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 
-def CreatePieChart(PathToFile):
+
+# Function to generate a Pie chart 
+# By reading the CSV file
+def create_pie_chart(path_to_file):
     # Load the dataset from the CSV file
-    data = pd.read_csv(PathToFile)
+    data = pd.read_csv(path_to_file)
 
-    # Extract data for the specific year
-    percent = data.iloc[:,1]
+    # Get the data of Percentages
+    percent = data.iloc[:, 1]  # Percentages are in the 1st column.
 
-    # Extract country codes
-    typeofFood = data['Food']
+    # Get the data of Food
+    typeof_food = data['Food']
 
-    # Creating a pie chart
+    # Creating a pie chart using the data.
     plt.figure(figsize=(8, 6))
-    plt.pie(percent, labels=typeofFood)
-    plt.title('Recommended Diet Chart')
+    plt.pie(percent, labels=typeof_food)
+    plt.title('Energy provided by major food group in 2000-2002')
+
+    # Display the plot
     plt.show()
 
-# Replace 'path_to_your_csv_file.csv' with the actual path to your CSV file
-PathToFile = r'C:\Users\Avinash Reddy\Videos\ADS1Assgn\Diet_Chart.csv'
-CreatePieChart(PathToFile)
+
+# Replace 'path_to_file.csv' with the actual path to your CSV file
+path_to_file = r'C:\Users\batta\OneDrive\Desktop\ads1\assignments1\Energy_Chart.csv'
+create_pie_chart(path_to_file)
